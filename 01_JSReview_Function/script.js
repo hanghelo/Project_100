9d4ede0c26790cac6933f89314bec8e4ded4d2b6
Gelo Reyes <reyesgelo00@gmail.com>
2025-03-22 14:47:38 +0800

    Add: Added my review function

---
+++ 01_JSReview_Function/script.js
@@ -0,0 +1,16 @@
+//Define the Function
+function BMI_Calculator (weight, height) {
+    const BMI_Value = weight / (height * height);
+    return BMI_Value;
+}
+
+//Input of the Function
+let user_Name = prompt ('Enter your name: ');
+let user_weight = Number(prompt ('Enter your weight: '));
+let user_height = Number(prompt ('Enter your height: '));
+
+//Calling the Function
+let computed_BMI = BMI_Calculator(user_weight, user_height);
+
+//Consoling the Function
+console.log (`Hi ${user_Name}! Your BMI is ${computed_BMI.toFixed(2)}`);
