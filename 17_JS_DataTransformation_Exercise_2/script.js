6771eb411b5f6f189c339d7cd9599b5de27c8d80
Gelo Reyes <reyesgelo00@gmail.com>
2025-05-25 19:41:14 +0800

    Add: Added exercise from 17_JS_DataTransformation_Exercise_2

--- 17_JS_DataTransformation_Exercise_2/script.js
+++ 17_JS_DataTransformation_Exercise_2/script.js
@@ -1,4 +1,4 @@
-const fruits = [
+const fruitBasket = [
     "Apple",
     "Banana",
     "Orange",
@@ -11,7 +11,32 @@ const fruits = [
     "Peach"
 ];
 
-// Add Fruits Function
-const add_fruit = fruit => {
-    
+// Displaying the fruit basket
+function display_fruitBasket () {
+    fruitBasket.forEach ( fruit => console.log (fruit));
+    // For each fruit in the fruitbasket, console.log fruit
+    // MY-NOTES: PRINT FRUIT IN THE FRUIT BASKET USING FOREACH
 }
+display_fruitBasket ();
+
+// Add a fruit in the fruit basket
+function add_fruit () {
+    const fruitToAdd = prompt ('Fruit to Add: ');
+    console.log (`You've added ${fruitToAdd}`);
+
+    fruitBasket.push (fruitToAdd);
+    display_fruitBasket ();
+}
+add_fruit ();
+
+
+// Remove a fruit
+function remove_fruit () {
+    const fruitToRemove = fruitBasket.pop ();
+    console.log (`You've removed ${fruitToRemove}`);
+
+    
+    display_fruitBasket ();
+}
+
+remove_fruit ();
