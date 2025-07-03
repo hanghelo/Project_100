bf293771453cebd28beafea09dd40bb7cf06d203
Gelo Reyes <reyesgelo00@gmail.com>
2025-07-03 22:45:39 +0800

    Add: Added the h2 - printing the color hex value as its value

--- 21_JS_DOM_UpdatingCSS-ChangingBackground/script.js
+++ 21_JS_DOM_UpdatingCSS-ChangingBackground/script.js
@@ -1,10 +1,15 @@
-//Goal: To create a button that will change the background color everytime it will be clicked
+//Goal: 
+// To create a button that will change the background color everytime it will be clicked
+// Display the generated hex color value to h2
 
 // MY-NOTES: 01. Select the Button
 const button = document.querySelector("button");
 
 // MY-NOTES: 02. Select the Background
 const background = document.querySelector("body");
+
+// MY-NOTES: 03. Select the H2
+const colorValue = document.querySelector("h2");
 
 button.addEventListener("click", ()=>{
     let color = "#";
@@ -13,8 +18,19 @@ button.addEventListener("click", ()=>{
     // Math.random() generates a random decimal number between 0 (inclusive) and 1 (exclusive), e.g., 0.123456789.
     // .toString(16) converts that number to a hexadecimal string, e.g., 0.123456789 becomes "0.1f9acf0798".
     // slice trims the initial generated value (ex: #0.1f9acf0798) from index 2 up to index 8 leaving the value to #1f9acf0
+
     console.log(color);
+    // just for validation
 
 
     background.style.backgroundColor = color;
+    // assign the clean random hexadecimal color to the css
+    // you can check it through the inspect elements
+
+
+    // h2 color assignment
+    colorValue.textContent = color;
+    // displays and assigns the value to h2
 })
+
+
