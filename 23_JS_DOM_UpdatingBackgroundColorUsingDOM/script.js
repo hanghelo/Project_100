8c94a96cae396ac3bcb70e2c75b70db0f1bef740
Gelo Reyes <reyesgelo00@gmail.com>
2025-07-12 16:23:52 +0800

    Add: Added Exercise 23_JS_DOM_UpdatingBackgroundColorUsingDOM

---
+++ 23_JS_DOM_UpdatingBackgroundColorUsingDOM/script.js
@@ -0,0 +1,7 @@
+// MY-NOTES: 01. Select the HTML Box
+const selectTheColor = document.querySelector ("color");
+const backgroundColor = document.querySelector ("body");
+const colorvalue = document.querySelectorAll("color")
+
+
+console.log (selectTheColor);
