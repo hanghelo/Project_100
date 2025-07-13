7734bb2eae634dc9ae3b855729a01e8d09961f1c
Gelo Reyes <reyesgelo00@gmail.com>
2025-07-14 00:58:54 +0800

    Update: Completed the initial requirement for 23_JS_DOM_UpdatingBackgroundColorUsingDOM

--- 23_JS_DOM_UpdatingBackgroundColorUsingDOM/script.js
+++ 23_JS_DOM_UpdatingBackgroundColorUsingDOM/script.js
@@ -1,7 +1,13 @@
-// MY-NOTES: 01. Select the HTML Box
-const selectTheColor = document.querySelector ("color");
-const backgroundColor = document.querySelector ("body");
-const colorvalue = document.querySelectorAll("color")
+// Select the div boxes to get the color value
+const boxes = document.querySelectorAll ('.colorbox');      // 1. Select all the boxes with class .colorbox 
+console.log (boxes);                                        // 1.1 Pangcheck lang - Returns a NodeList (6) since there is 6 divs //Output: i: div.colorbox.color
 
+boxes.forEach( boxToselect => {                                     // 2. Code here will run for EACH box. Applyan ng code each box
+    boxToselect.addEventListener('click', () => {                     // 2.1 In each box, assign an event listener
+    const color = boxToselect.textContent.trim().toLowerCase();     // 2.2 Formats the textcontent value
+    console.log (color);                                            // 2.3 Pang-check lang na kapag clinick yung div ay magcoconsole log yung 
 
-console.log (selectTheColor);
+    boxToselect.style.backgroundColor = color ;                     // 2.4 Select the i or the boxToSelect (the callback parameter) and assign the color to every div
+    boxToselect.style.color = 'white';                              // 2.5 Select the text color and assign white to every div
+    });
+});
